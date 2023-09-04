951416b9ca64db545d57a40eb42b8f2103b54b49
bigbarin69 <prathik.prejith@gmail.com>
2023-09-04 15:30:21 +0530

    Final register page

--- lib/register.dart
+++ lib/register.dart
@@ -46,57 +46,92 @@ class _MyRegisterState extends State<MyRegister> {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Test Register',
+      theme: ThemeData(
+        fontFamily: 'Interbold',
+        scaffoldBackgroundColor: const Color.fromARGB(175, 104, 208, 189),
+      ),
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
+          backgroundColor: const Color.fromARGB(178, 70, 164, 196),
           centerTitle: true,
-          title: const Text('Test Register'),
-          leading: TextButton(
-            onPressed: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => const MyApp()));
-            },
-            child: const Text(
-              'Home',
-              style: TextStyle(color: Colors.white),
-            ),
+          titleSpacing: 20.0,
+          toolbarHeight: 70.0,
+          title: const Text(
+            'Test Register',
+            style: TextStyle(fontSize: 25.0),
           ),
+          leading: Row(
+            children: <Widget>[
+              const SizedBox(width: 20.0),
+              Container(
+                padding: const EdgeInsets.all(3.0),
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.white),
+                    borderRadius: const BorderRadius.all(Radius.circular(5.0))),
+                child: TextButton(
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => const MyApp()));
+                  },
+                  child: const Text(
+                    'Home',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          leadingWidth: 100.0,
         ),
-        body: Container(
-          alignment: Alignment.center,
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                TextField(
-                  controller: myFullname,
-                  decoration: const InputDecoration(hintText: 'Full Name'),
-                ),
-                TextField(
-                  controller: myUsername,
-                  decoration: const InputDecoration(hintText: 'Username'),
-                ),
-                TextField(
-                  controller: myPassword,
-                  decoration: const InputDecoration(hintText: 'Password'),
-                ),
-                TextField(
-                  controller: myEmail,
-                  decoration: const InputDecoration(hintText: 'Email'),
-                ),
-                TextField(
-                  controller: myPhoneno,
-                  keyboardType: TextInputType.number,
-                  decoration: const InputDecoration(hintText: 'Phone Number'),
-                ),
-                ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        createregister(myUsername.text, myPassword.text,
-                            myFullname.text, myEmail.text, myPhoneno.text);
-                      });
-                    },
-                    child: const Text('Register'))
-              ]),
+        body: Center(
+          child: Container(
+            height: 500.0,
+            width: 450.0,
+            padding: const EdgeInsets.all(15.0),
+            decoration: BoxDecoration(
+                border: Border.all(color: Colors.blueGrey, width: 3.0),
+                borderRadius: const BorderRadius.all(Radius.circular(15.0))),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  TextField(
+                    controller: myFullname,
+                    decoration: const InputDecoration(hintText: 'Full Name'),
+                  ),
+                  TextField(
+                    controller: myUsername,
+                    decoration: const InputDecoration(hintText: 'Username'),
+                  ),
+                  TextField(
+                    controller: myPassword,
+                    decoration: const InputDecoration(hintText: 'Password'),
+                  ),
+                  TextField(
+                    controller: myEmail,
+                    decoration: const InputDecoration(hintText: 'Email'),
+                  ),
+                  TextField(
+                    controller: myPhoneno,
+                    keyboardType: TextInputType.number,
+                    decoration: const InputDecoration(hintText: 'Phone Number'),
+                  ),
+                  const SizedBox(
+                    height: 30.0,
+                  ),
+                  ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor:
+                              const Color.fromARGB(255, 10, 187, 161)),
+                      onPressed: () {
+                        setState(() {
+                          createregister(myUsername.text, myPassword.text,
+                              myFullname.text, myEmail.text, myPhoneno.text);
+                        });
+                      },
+                      child: const Text('Register'))
+                ]),
+          ),
         ),
       ),
     );
