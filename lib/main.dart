14c4d880e02efd02a769d73ddb71fe05636d4e21
bigbarin69 <prathik.prejith@gmail.com>
2023-09-04 14:28:41 +0530

    Final home page except logo

--- lib/main.dart
+++ lib/main.dart
@@ -20,62 +20,141 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Home Page',
+      title: 'Akashic',
+      theme: ThemeData(
+        fontFamily: 'Interbold',
+        scaffoldBackgroundColor: const Color.fromARGB(175, 104, 208, 189),
+      ),
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-          appBar: AppBar(
-        title: const Text('Home Page'),
-        actions: <Widget>[
-          Row(
-            children: <Widget>[
-              TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const MyLogin()));
-                  },
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(178, 70, 164, 196),
+          titleSpacing: 20.0,
+          toolbarHeight: 70.0,
+          title: const Text(
+            'Akashic',
+            style: TextStyle(
+              fontSize: 40.0,
+              fontFamily: 'Lily',
+              letterSpacing: 1.5,
+            ),
+          ),
+          actions: <Widget>[
+            Row(
+              children: <Widget>[
+                Container(
+                  padding: const EdgeInsets.all(3.0),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.white),
+                      borderRadius:
+                          const BorderRadius.all(Radius.circular(5.0))),
+                  child: TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const MyLogin()));
+                      },
+                      child: const Text(
+                        'Login',
+                        style: TextStyle(color: Colors.white),
+                      )),
+                ),
+                const SizedBox(
+                  width: 30.0,
+                ),
+                Container(
+                  padding: const EdgeInsets.all(3.0),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.white),
+                      borderRadius:
+                          const BorderRadius.all(Radius.circular(5.0))),
+                  child: TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const MyRegister()));
+                      },
+                      child: const Text(
+                        'Register',
+                        style: TextStyle(color: Colors.white),
+                      )),
+                ),
+                const SizedBox(
+                  width: 30.0,
+                ),
+                Container(
+                  padding: const EdgeInsets.all(3.0),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.white),
+                      borderRadius:
+                          const BorderRadius.all(Radius.circular(5.0))),
+                  child: TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const Dashboard()));
+                      },
+                      child: const Text(
+                        'Dashboard',
+                        style: TextStyle(color: Colors.white),
+                      )),
+                ),
+                const SizedBox(
+                  width: 30.0,
+                ),
+                Container(
+                  padding: const EdgeInsets.all(3.0),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.white),
+                      borderRadius:
+                          const BorderRadius.all(Radius.circular(5.0))),
+                  child: TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const Profile()));
+                      },
+                      child: const Text(
+                        'Profile',
+                        style: TextStyle(color: Colors.white),
+                      )),
+                ),
+                const SizedBox(
+                  width: 20.0,
+                ),
+              ],
+            )
+          ],
+        ),
+        body: Row(
+          children: <Widget>[
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  padding: const EdgeInsets.fromLTRB(55.0, 20.0, 0, 10.0),
                   child: const Text(
-                    'Login',
-                    style: TextStyle(color: Colors.white),
-                  )),
-              TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const MyRegister()));
-                  },
+                    'Welcome!',
+                    style: TextStyle(fontSize: 40.0),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.fromLTRB(55.0, 20.0, 0, 10.0),
                   child: const Text(
-                    'Register',
-                    style: TextStyle(color: Colors.white),
-                  )),
-              TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const Dashboard()));
-                  },
-                  child: const Text(
-                    'Dashboard',
-                    style: TextStyle(color: Colors.white),
-                  )),
-              TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const Profile()));
-                  },
-                  child: const Text(
-                    'Profile',
-                    style: TextStyle(color: Colors.white),
-                  ))
-            ],
-          )
-        ],
-      )),
+                    'Akashic is a free to use database app that you\ncan use to keep track of any and all books\nyouve read.Over 200,000 books and written material\nall in one location. Sign up today to get access\nto a mega-library at the cost of free!\n',
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
